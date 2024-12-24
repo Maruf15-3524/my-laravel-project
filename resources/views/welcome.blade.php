171f9ee0588ffcc86187d2cc046efb7b5bc0a4f2
Maruf15-3524 <maarufhasan4@gmail.com>
2024-12-24 16:13:32 +0600

    add new page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,20 @@
     <?php
         echo "Hello, World!";
         ?>
-        <button click="add_somthing()">
+        <button click="add_somthing()" style="btn btn-small btn-success">
             Click me     </button>
 </body>
 </html>
 <script>
     function add_somthing(){
         alert("Hello, World!");
+        $.ajax({
+          type: "POST",
+          url: "module//result_sms_ajax.php",
+          data: datastra,
+          success: function(result) {
+            alert(result);
+          }
+        });
     }
 </script>
